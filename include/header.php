72c59e4cc82b0d1ceab661786807fde9be413908
BlueberryCrunch <a.j.muehlbauer@gmail.com>
2016-04-19 17:20:39 +0200

    Grober Schnitzer

--- include/header.php
+++ include/header.php
@@ -11,7 +11,7 @@
 <head>
     <meta charset = "utf-8" name="viewport" content="width = device-width, initial-scale = 1">
     <meta charset = "utf-8" name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"
     <link rel = "stylesheet" href = "css/style.css">
     <script src="js/boostrap.min.js"></script>
     <script src="js/boostrap.js"></script>
