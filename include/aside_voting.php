b3a59384b86225547c969b98b6893bf083eb4f8c
migreet <mic.graeter@googlemail.com>
2016-04-21 19:07:12 +0200

    frage und antwort createn

--- include/aside_voting.php
+++ include/aside_voting.php
@@ -1,14 +1,14 @@
 <?php
 
 $frageCreate=$_POST["fragecreate"];
-
+$frageText = htmlspecialchars($_POST["frage"], ENT_QUOTES, "UTF-8");
 // echo $ID_Voting;
 
 if (isset($frageCreate)) {
-    $frageText = htmlspecialchars($_POST["frage"], ENT_QUOTES, "UTF-8");
-    if (!empty ($bezeichnung)) {
+
+    if (!empty ($frageText)) {
         $frageInstnc = new frage(); //richtige Klasse benutzen!!!    createFrage($bezeichnung, $text, $votingid)
-        $frage = $frageInstnc->createFrage($bezeichnung, $frageText, $ID_Voting);
+        $frage = $frageInstnc->createFrage($frageText, $ID_Voting);
 
         echo "<div> Die Frage wurde eingereicht</div>";
 
