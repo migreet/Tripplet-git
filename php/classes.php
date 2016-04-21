1dbd5bea9f4f45e99fa9c13426fafb29cc480f89
migreet <mic.graeter@googlemail.com>
2016-04-21 16:40:29 +0200

    output

--- php/classes.php
+++ php/classes.php
@@ -372,6 +372,7 @@ class frage extends mother
             $stmt = $this->pdo->prepare('
               	INSERT INTO frage (bezeichnung, text, ID_VOTING)
 				VALUES (:bezeichnung,:text,:votingid)
+				OUTPUT INSERTED.ID VALUES (?)
             ');
             $stmt->bindParam(':bezeichnung', $bezeichnung);
             $stmt->bindParam(':text', $text);
