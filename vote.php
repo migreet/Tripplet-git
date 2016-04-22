813bc1bb14fe2c0933023c871a986aa044d45aae
migreet <mic.graeter@googlemail.com>
2016-04-22 22:08:14 +0200

    header weg bug lösung

--- vote.php
+++ vote.php
@@ -36,9 +36,8 @@ print_r($frage);
 
 if (isset($_POST["fragerunde"])) {
     $eintragID = htmlspecialchars($_POST['antwort'], ENT_QUOTES, "UTF-8");
-    echo $eintragID;
-    $auswertungInstnc= new auswertung();
-    $auswertungupdate= $auswertungInstnc->update(63 , $_SESSION['id'], $eintragID);
+    echo $eintragID. $fragerunde['ID_FRAGE']. $_SESSION['id']. $eintragID;
+    $auswertungupdate= $auswertungInstnc->update($fragerunde['ID_FRAGE'], $_SESSION['id'], $eintragID);
     //header ('location:vote.php');
 }
 
