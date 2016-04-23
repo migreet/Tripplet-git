3bc697600b4b71b15a0d2e4c51da8d8c79206bb2
BlueberryCrunch <a.j.muehlbauer@gmail.com>
2016-04-24 01:37:02 +0200

    login erweiterung um rechte

--- vorlesung.php
+++ vorlesung.php
@@ -65,39 +65,36 @@ $vorlesung=$vorlesungInstnc->getById($ID_Vorlesung);
     $voting = $votingInstnc->getByVorlesungsId($ID_Vorlesung);
 if (!empty ($voting)):
     foreach ($voting as $eintrag) {
-    echo "
-    <div class='list-entry'>
-    <div class='col-md-7'>";
-    if (empty($eintrag['schluessel'])){
-    echo "<div style='color:red'>";
-    echo "<a href='voting.php?id=".$eintrag['ID']."'>";
-    echo $eintrag['bezeichnung'] . " ";
-    echo $eintrag['datum'] . " ";
-    echo "</a> ";?>
-    <a href="start.php?id=<?php echo $eintrag['ID']?>" class="btn btn-success">Start</a>
-    <button type='button' class='btn btn-info'>edit</button>
-    <!-- Votings löschen -->
-    <a href="do/vorlesung_delete.php?id=<?php echo $eintrag['ID']. '&' .'idvorlesung='.$ID_Vorlesung;?>" class="btn btn-danger">Löschen</a>
-    </div>
+        echo "<div class='list-entry'>
+              <div class='col-md-7'>";
+        if (empty($eintrag['schluessel'])){
+            echo "<div style='color:red'>";
+            echo "<a href='voting.php?id=".$eintrag['ID']."'>";
+            echo $eintrag['bezeichnung'] . " ";
+            echo $eintrag['datum'] . " ";
+            echo "</a> ";?>
+            <a href="start.php?id=<?php echo $eintrag['ID']?>" class="btn btn-success">Start</a>
+            <button type='button' class='btn btn-info'>edit</button>
+            <!-- Votings löschen -->
+            <a href="do/vorlesung_delete.php?id=<?php echo $eintrag['ID']. '&' .'idvorlesung='.$ID_Vorlesung;?>" class="btn btn-danger">Löschen</a>
+            </div>
+    <?php
+        }
+    else{
+        echo "<div style='color:green'>";
+        echo "<a href='voting.php?id=".$eintrag['ID']."'>";
+        echo $eintrag['bezeichnung'] . " ";
+        echo $eintrag['datum'] . " ";
+        echo "</a> ";?>
+        <a href="start.php?id=<?php echo $eintrag['ID']?>" class="btn btn-success">Start</a>
+        <button type='button' class='btn btn-info'>edit</button>
+        <!-- Votings löschen -->
+        <a href="do/vorlesung_delete.php?id=<?php echo $eintrag['ID']. '&' .'idvorlesung='.$ID_Vorlesung;?>" class="btn btn-danger">Löschen</a>
+        </div>
+        </div>
+        </div>
 <?php
-}
-else{
-
-echo "<div style='color:green'>";
-echo "<a href='voting.php?id=".$eintrag['ID']."'>";
-echo $eintrag['bezeichnung'] . " ";
-echo $eintrag['datum'] . " ";
-echo "</a> ";?>
-<a href="start.php?id=<?php echo $eintrag['ID']?>" class="btn btn-success">Start</a>
-<button type='button' class='btn btn-info'>edit</button>
-<!-- Votings löschen -->
-<a href="do/vorlesung_delete.php?id=<?php echo $eintrag['ID']. '&' .'idvorlesung='.$ID_Vorlesung;?>" class="btn btn-danger">Löschen</a>
-</div>
-</div>
-</div>
-
-}
-
+    }
 }
 else:
     echo "Es sind keine Votings Vorhanden";
