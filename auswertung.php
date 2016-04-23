7ea8ce4f4ca9416b6e9768504e66273492641719
BlueberryCrunch <a.j.muehlbauer@gmail.com>
2016-04-24 01:55:14 +0200

    seite auswertung erstellen

--- auswertung.php
+++ auswertung.php
@@ -100,7 +100,7 @@ if (isset($postVoting)) {
             <?php
 
             //Ausgeben der Fragen zur passenden Voting ID
-            echo "<p><strong>Auswertung XY</strong></p>";
+            echo "<p><strong>Statistiken im Überblick</strong></p>";
             $voting = $frageInstnc->getByVotingId($ID_Voting);
             if (!empty ($voting)):
             foreach ($voting as $eintrag) {
