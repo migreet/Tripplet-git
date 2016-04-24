f823d2d636a4408e3e84f52ca8dae7b0a611f790
BlueberryCrunch <a.j.muehlbauer@gmail.com>
2016-04-24 13:30:14 +0200

    antworten zählen

--- admin.php
+++ admin.php
@@ -33,7 +33,7 @@
                     }
                         echo "<a href='admin.php?id=" . $eintrag['ID'] . "'class='btn btn-success'>Löschen</a>";
 
-                    if ($_SESSION['rights']==3 && !empty($user['ID_RECHTE'])){
+                    if ($_SESSION['rights']==3 && $user['ID_RECHTE']!=NULL){
 
                         echo "<a href='admin.php?id=" . $eintrag['ID'] . "'class='btn btn-success'>Grant Admin</a>";
                     }
