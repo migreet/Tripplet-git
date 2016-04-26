9a60960cccc687d3bd6e246b776bb47eeaa4f67b
BlueberryCrunch <a.j.muehlbauer@gmail.com>
2016-04-26 15:02:05 +0200

    grafische darstellung auswertung

--- auswertung.php
+++ auswertung.php
@@ -121,7 +121,7 @@ if (isset($postVoting)) {
 
                 echo $eintragFrage['text'];
                 $anzahlAntworten=$auswertungInstnc->countAntworten($eintragFrage['ID']);
-                $percent=100/int($eintrag)*int($anzahlAntworten['COUNT(*)']);
+                $percent=100/intval($eintrag)*intval($anzahlAntworten['COUNT(*)']);
                 echo $percent;
 
             }
