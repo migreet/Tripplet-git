72b5b9811f1acd9a202e2dbe64b0e45caea8a219
migreet <mic.graeter@googlemail.com>
2016-04-27 08:54:44 +0200

    google font hinzufügen

--- include/header.php
+++ include/header.php
@@ -11,6 +11,7 @@
 <head>
     <meta charset = "utf-8" name="viewport" content="width = device-width, initial-scale = 1">
     <meta charset = "utf-8" name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,500,700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel = "stylesheet" href = "css/style.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
