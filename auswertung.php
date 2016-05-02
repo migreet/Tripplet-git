ebc3146207ccd890f03d497f6516e6cd20c0bbc6
BlueberryCrunch <a.j.muehlbauer@gmail.com>
2016-05-02 13:32:30 +0200

    grafische darstellung auswertung

--- auswertung.php
+++ auswertung.php
@@ -119,7 +119,10 @@ if (isset($postVoting)) {
             $antwort=$antwortInstnc->getByFragenId($eintrag['ID']);
             foreach ($antwort as $eintragFrage) {
                 $anzahlAntworten=$auswertungInstnc->countAntworten($eintragFrage['ID']);
+                $percent = 0;
+                if(intval($anzahlTeilnehmer['COUNT(*)']!=0)) {
                 $percent=round(100/intval($anzahlTeilnehmer['COUNT(*)'])*intval($anzahlAntworten['COUNT(*)']),2);
+                }
                 echo $eintragFrage['text'] ." (Stimmen: ". $anzahlAntworten['COUNT(*)']." | ".$percent."%)";
                 echo "
                 <div class='progress'>
