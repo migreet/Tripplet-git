35e8d4063372d6a3b5cfaaa50495d826271aa285
BlueberryCrunch <a.j.muehlbauer@gmail.com>
2016-05-02 23:47:13 +0200

    Auskommentierung Klassen

--- do/clear.php
+++ do/clear.php
@@ -5,3 +5,11 @@
  * Date: 02.05.2016
  * Time: 23:22
  */
+
+//Requires
+require_once("php/classes.php");
+require_once("include/header.php");
+session_start();
+
+$votingInstnc = new voting();
+$voting = $votingInstnc ->getByTimestamp();
