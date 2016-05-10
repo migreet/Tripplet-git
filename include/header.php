104864098821f5b0afd750ba6809c08208922838
BlueberryCrunch <a.j.muehlbauer@gmail.com>
2016-05-10 11:51:01 +0200

    funktion um alte votings auszugeben

--- include/header.php
+++ include/header.php
@@ -13,6 +13,7 @@
     <meta charset = "utf-8" name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <link href='https://fonts.googleapis.com/css?family=Roboto:400,500,700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">
     <link rel = "stylesheet" href = "css/style.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <title>Trippletime</title>
