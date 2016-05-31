f3e9b9e76091307bff99b2438e253e0f1745d818
migreet <mic.graeter@googlemail.com>
2016-05-31 17:03:06 +0200

    ids hinzufügen aside-index

--- vote.php
+++ vote.php
@@ -142,7 +142,7 @@ if (isset($_SESSION['id']) && isset($_SESSION['votingid'])):
             echo "</div>
 
         <div class='col-xs-12 col-sm-12 col-md-1'><a href='vote_logout.php' class='btn btn-danger vote-btn'>Ausloggen</a></div>
-            <div class='col-xs-12 col-sm-12 col-md-1'>";
+            <div class='col-xs-12 col-sm-12 col-md-1 vote-dot'>";
             $anzahlFragen=$countFragen['COUNT(*)'];
             $anzahlFragenready=$countfinished ['COUNT(*)'];
             for ($i = 0; $i < $anzahlFragen; $i++)
