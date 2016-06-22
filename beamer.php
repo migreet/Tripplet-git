577fde5f8e421a1b3f5fe7c067742b33720034e8
BlueberryCrunch <a.j.muehlbauer@gmail.com>
2016-06-22 16:39:18 +0200

    styling login form vote.php

--- beamer.php
+++ beamer.php
@@ -26,7 +26,7 @@ echo "<div id='beamer' class='container'>";
             ?>
 
             <!--Ausgabe der Antworten zu der passenden Frage-->
-            <div class="col-md-12" style="clear:both;">
+
             <?php
             $antwort=$antwortInstnc->getByFragenId($frage['ID']);
             foreach ($antwort as $eintragFrage) {
@@ -35,7 +35,7 @@ echo "<div id='beamer' class='container'>";
                 if(intval($anzahlTeilnehmer['COUNT(*)']!=0)) {
                 $percent=round(100/intval($anzahlTeilnehmer['COUNT(*)'])*intval($anzahlAntworten['COUNT(*)']),2);
                 }
-                echo "<h3 style='float:left; '>".$eintragFrage['text'] ." (Stimmen: ". $anzahlAntworten['COUNT(*)']." | ".$percent."%)"."</h3> ";
+                echo "<div class='col-md-12'><h3 style='float:left; '>".$eintragFrage['text'] ." (Stimmen: ". $anzahlAntworten['COUNT(*)']." | ".$percent."%)"."</h3> ";
                 echo "
                 <div class='progress' style='margin-top: 20px; margin-bottom: 10px;height:40px;'>
                     <div class='progress-bar progress-bar-info' role='progressbar' aria-valuenow='$percent' aria-valuemin='0' aria-valuemax='100' style='background-color:#55d6ba; width: $percent%'>
