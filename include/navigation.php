ff750972060f4a0680f8ce6e26cba0a58eff2cdf
BlueberryCrunch <a.j.muehlbauer@gmail.com>
2016-06-22 13:34:42 +0200

    styling login form vote.php

--- include/navigation.php
+++ include/navigation.php
@@ -9,7 +9,7 @@
 
 <nav class="navbar navbar-inverse">
     <div class="container">
-    <ul class="nav navbar-nav">
+    <ul class="nav navbar-nav" style="float: right">
         <li>
             <a href="admin.php" >Accountverwaltung</a>
         </li>
