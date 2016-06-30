fb4e11c36bd954a6a776d58b06e87d28af370ba8
migreet <mic.graeter@googlemail.com>
2016-06-30 20:05:36 +0200

    impressum

--- impressum.php
+++ impressum.php
@@ -13,14 +13,15 @@ session_start();
     <body>
     <?php require_once("include/navigation.php");
     ?>
-    <?php
-    //Breadcrumb
-    echo"
+
+    <div class="container">
+        <?php
+        //Breadcrumb
+        echo"
         <div class='breadcrumb'>
         > <a href='index.php'> Vorlesungen</a> > Accountverwaltung
         </div>";
-    ?>
-    <div class="container">
+        ?>
         <h1>Impressum</h1>
         <div>Hochschule der Medien<br>
             Nobelstraße 10 <br>
