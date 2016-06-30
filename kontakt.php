561cf623de895e416e8e56cefbdb11614188aa91
migreet <mic.graeter@googlemail.com>
2016-06-30 20:41:27 +0200

    Kontakt

---
+++ kontakt.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="de">
+
+
+<?php
+require_once("include/header.php");
+require_once("php/classes.php");
+
+session_start();
+?>
+
+
+<body>
+<?php require_once("include/navigation.php");
+?>
+
+<div class="container">
+    <?php
+    //Breadcrumb
+    echo"
+        <div class='breadcrumb'>
+        > <a href='index.php'> Vorlesungen</a> > Impressum
+        </div>";
+    echo"<h1>Impressum</h1>";
+    echo "<div class='col-md-8'>";
+
+    echo "</div>";
+
+    echo "<div class='col-md-4' id='impressim_aside'> ";
+
+    echo "</div>";
+    ?>
+
+</div>
+
+
+
+</div>
+
+</body>
+
+
+</html>
+
