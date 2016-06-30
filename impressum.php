00310cc38c4fe0cdac2c2ec75c5e775ae355d5da
migreet <mic.graeter@googlemail.com>
2016-06-30 19:58:40 +0200

    impressum

--- impressum.php
+++ impressum.php
@@ -1,7 +1,41 @@
-<div class="container">
-<h1>Impressum</h1>
-<div>Hochschule der Medien<br>
-     Nobelstraﬂe 10 <br>
-     70569 Stuttgart
-</div>
-</div>
+<!DOCTYPE html>
+<html lang="de">
+
+
+<?php
+require_once("include/header.php");
+require_once("php/classes.php");
+
+session_start();
+?>
+
+
+    <body>
+    <?php require_once("include/navigation.php");
+    ?>
+
+    <div class="container">
+        <h1>Impressum</h1>
+        <div>Hochschule der Medien<br>
+            Nobelstra√üe 10 <br>
+            70569 Stuttgart
+        </div>
+    </div>
+        <?php
+        //Breadcrumb
+        echo"
+        <div class='breadcrumb'>
+        > <a href='index.php'> Vorlesungen</a> > Accountverwaltung
+        </div>";
+        ?>
+
+
+                </div>
+
+    </body>
+
+<?php endif; ?>
+</html>
+
+
+
