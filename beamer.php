ce1ddf19ebf8e2d9951989ddb1c77877b7dec62e
migreet <mic.graeter@googlemail.com>
2016-07-01 13:45:15 +0200

    footer einbindung

--- beamer.php
+++ beamer.php
@@ -18,7 +18,7 @@ $auswertungInstnc = new auswertung();
 $ID_Frage=$_GET['id'];
 $frage=$frageInstnc->getById($ID_Frage);
 
-echo "<div id='beamer' class='container' id='beamer'>";
+echo "<div id='beamer' class='container'>";
 
             $anzahlTeilnehmer=$auswertungInstnc->countTeilnehmer($frage['ID']);
             echo "<h2>" . $frage['text'] . "(" . $anzahlTeilnehmer['COUNT(*)'] . " Teilnehmer)"."</h2></br> ";
@@ -38,7 +38,7 @@ echo "<div id='beamer' class='container' id='beamer'>";
                 echo "<div class='col-md-12 beamer'><h3 class='col-md-6 h3-beamer'>".$eintragFrage['text'] ." (Stimmen: ". $anzahlAntworten['COUNT(*)']." | ".$percent."%)"."</h3> ";
                 echo "
                 <div class='col-md-6 progress' style='margin-top: 20px; margin-bottom: 10px;height:40px; padding-left: 0; padding-right: 0;'>
-                    <div class='progress-bar progress-bar-info' role='progressbar' aria-valuenow='$percent' aria-valuemin='0' aria-valuemax='100' style='width: $percent%'>
+                    <div class='progress-bar progress-bar-info' role='progressbar' aria-valuenow='$percent' aria-valuemin='0' aria-valuemax='100' style='background-color:#55d6ba; width: $percent%'>
                 <span class='sr-only'>20% Complete</span>
                 </div>
                 </div>
