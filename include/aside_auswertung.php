c23e799de6236e3771a72357cc322ab707c65d9d
migreet <mic.graeter@googlemail.com>
2016-07-01 17:45:31 +0200

    aside auswertung.php

--- include/aside_auswertung.php
+++ include/aside_auswertung.php
@@ -1,6 +1,7 @@
 <?php
 echo"<div>
             <div>
+           <p><strong>Statistiken im Überblick</strong></p>
 In diesem Bereich werden Votings geöffnet und geschlossen. Wählen Sie einen Schlüssel um den Zugang zu Ihrem Voting zu beschränken.
             </div>
         </div>";
