880d0ecc7aa4855461597803ac1ac654cd35da3c
migreet <mic.graeter@googlemail.com>
2016-07-01 17:02:45 +0200

    voting php fix

--- voting.php
+++ voting.php
@@ -106,7 +106,7 @@ if (isset($postVoting)) {
         <i class='fa fa-angle-right'></i> <a href='index.php'>Vorlesungen</a> <i class='fa fa-angle-right'></i> <a href='vorlesung.php?id=".$vorlesung['ID'] ."'> ". $vorlesung['bezeichnung']. "</a> <i class='fa fa-angle-right'></i> ". $voting['bezeichnung'] ." bearbeiten
         </div>";
         //neu eingefügt
-
+        echo"</div>";
         ?>
 
         <h1> Voting <?php echo $voting['bezeichnung']; ?></h1>
@@ -124,7 +124,7 @@ if (isset($postVoting)) {
             if (!empty ($voting)):
             foreach ($voting as $eintrag) {
             echo "
-    <div class='list-entry'>
+
     <div class='col-md-7'>";
 
             echo " " . $eintrag['text'] . " ";
@@ -147,8 +147,7 @@ if (isset($postVoting)) {
         <!-- Fragenlöschen-->
         <a href="do/voting_delete.php?id=<?php echo $eintrag['ID']. '&' .'idvoting='.$ID_Voting;?>" class="btn btn-danger">Löschen</a>
         </div>
-        </div>
-        </div>
+
     <?php }
     else:
         echo "Es sind keine Fragen vorhanden";
