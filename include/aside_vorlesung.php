1e6e3577673365570fddbadcecb2b5c1caab1e37
migreet <mic.graeter@googlemail.com>
2016-07-01 19:55:05 +0200

    sidebar-left

--- include/aside_vorlesung.php
+++ include/aside_vorlesung.php
@@ -1,4 +1,24 @@
+<?php
+<form class="form-horizontal" role="form" action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+            <div class="form-group">
+                <div class="col-sm-12">
+                    <input type="text" class="form-control" name="bezeichnung" placeholder="Bezeichnnung" id="bezeichnung" required>
+                </div>
+                <input type="hidden" value="1" name="votingcreate">
 
+
+
+
+
+
+                <div class="col-sm-12">
+                    <button type="submit" class="btn btn-default">hinzufügen</button>
+                </div>
+            </div>
+        </form>
+
+
+/*
 <div class="col-md-4 sidebar-left">
     Eine neues Voting hinzufügen!
 </div>
@@ -21,3 +41,6 @@
         </div>
     </div>
 </form>
+*/
+
+?>
