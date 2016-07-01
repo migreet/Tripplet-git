dc1db3160ace5821a93461cb7ee013b13027b12c
BlueberryCrunch <a.j.muehlbauer@gmail.com>
2016-07-01 19:17:22 +0200

    styling login form vote.php

--- include/aside_auswertung.php
+++ include/aside_auswertung.php
@@ -1,7 +1,6 @@
 <?php
 echo"<div>
             <div>
-           <p><strong>Statistiken im Überblick</strong></p>
 In diesem Bereich werden Votings geöffnet und geschlossen. Wählen Sie einen Schlüssel um den Zugang zu Ihrem Voting zu beschränken.
             </div>
         </div>";
--- include/aside_impressum.php
+++ include/aside_impressum.php
@@ -1,6 +1,5 @@
 <?php
 echo "
-        <strong><p>Adresse</p></strong>
         <div>Hochschule der Medien<br>
             Nobelstraße 10 <br>
             70569 Stuttgart
