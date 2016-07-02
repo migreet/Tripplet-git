26a27cca39a02ac24808ed420aa18ff3862ce39b
migreet <mic.graeter@googlemail.com>
2016-07-02 14:29:07 +0200

    sidebar fix start.php

--- include/aside_start.php
+++ include/aside_start.php
@@ -2,7 +2,7 @@
     <div class="panel-body">
 
 <?php
-echo"<div class='col-md-4 sidebar-left'>
+echo"<div class='sidebar-left'>
             <div class='side-text'>
 In diesem Bereich werden Votings geöffnet und geschlossen. Wählen Sie einen Schlüssel um den Zugang zu Ihrem Voting zu beschränken.
             </div>
