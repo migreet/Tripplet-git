d9d9854905dd0f5f23200d67933c1bce41a2e7f3
BlueberryCrunch <a.j.muehlbauer@gmail.com>
2016-07-02 14:32:19 +0200

    styling login form vote.php

--- php/classes.php
+++ php/classes.php
@@ -288,6 +288,32 @@ class vorlesung extends mother
         if (!$n) $n = null;
         return $n;
     }
+
+    public function userCheck($id)
+    {
+
+        try {
+            $stmt = $this->pdo->prepare('
+              SELECT * FROM dozent
+              INNER JOIN vorlesung
+              ON dozent.ID = vorlesung.ID_DOZENT
+              INNER JOIN voting
+              ON vorlesung.ID = voting.ID_VORLESUNG
+              INNER JOIN frage
+              ON voting.ID = frage.ID_VOTING');
+            $stmt->execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $n = $stmt->fetch();
+        } catch (PDOException $e) {
+            echo("Fehler! Bitten wenden Sie sich an den Administrator...<br>" . $e->getMessage() . "<br>");
+            die();
+        }
+        if (!$n) $n = null;
+        return $n;
+
+    }
+
+
 }
 
 class voting extends mother
