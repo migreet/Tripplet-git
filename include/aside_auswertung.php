0d0a9fb32bcc4f1882f287c16946610409744062
migreet <mic.graeter@googlemail.com>
2016-07-02 14:14:26 +0200

    sidebar fix overall

--- include/aside_auswertung.php
+++ include/aside_auswertung.php
@@ -3,7 +3,7 @@
 
 <?php
 echo"<div class='sidebar-left'>
-            <div>
+            <div class='side-text'>
 In diesem Bereich werden Votings geöffnet und geschlossen. Wählen Sie einen Schlüssel um den Zugang zu Ihrem Voting zu beschränken.
             </div>
         </div>";
--- include/aside_impressum.php
+++ include/aside_impressum.php
@@ -3,9 +3,11 @@
 
 <?php
 echo " <div class='sidebar-left'>
+ <div class='side-text'>
         <div>Hochschule der Medien<br>
             Nobelstraße 10 <br>
             70569 Stuttgart
+</div>
 </div>
 </div>";
 ?>
