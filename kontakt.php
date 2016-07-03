99f412e1c1c3d179e0bbd2702878f0250ac2f5aa
migreet <mic.graeter@googlemail.com>
2016-07-03 22:43:18 +0200

    code aufräumen

--- kontakt.php
+++ kontakt.php
@@ -1,44 +1,35 @@
 <!DOCTYPE html>
 <html lang="de">
-
+<body>
 
 <?php
+/**
+ * Requires
+ */
 require_once("include/header.php");
 require_once("php/classes.php");
+require_once("include/navigation.php");
 
+/**
+ * Session wird gestartet
+ */
 session_start();
 ?>
 
-
-<body>
-<?php require_once("include/navigation.php");
-?>
-
 <div class="container">
-    <?php
-    //Breadcrumb
-    echo"
-        <div class='breadcrumb'>
+    <!-- Breadcrumb -->
+    <div class='breadcrumb'>
         <i class='fa fa-angle-right'></i> <a href='index.php'> Vorlesungen</a> <i class='fa fa-angle-right'></i> Kontakt
-        </div>";
-    echo"<h1>Impressum</h1>";
-    echo "<div class='col-md-8'>";
-
-    echo "</div>";
-
-    echo "<div class='col-md-4' id='impressim_aside'> ";
-
-    echo "</div>";
-    ?>
-
+    </div>
+    <h1>Impressum</h1>
+    <div class='col-md-8'>
+    </div>
+    <div class='col-md-4' id='impressim_aside'>
+    </div>
 </div>
 
-
-
-</div>
 <?php require_once('include/footer.php'); ?>
+
 </body>
-
-
 </html>
 
