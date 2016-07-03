f6c9b57156b364b9761e660ff89b010a61b70adb
migreet <mic.graeter@googlemail.com>
2016-07-03 16:25:51 +0200

    code aufräumen

--- admin.php
+++ admin.php
@@ -1,33 +1,49 @@
 <!DOCTYPE html>
 <html lang="de">
-
+<body>
 
 <?php
+/**
+ * Requires
+ */
 require_once("include/header.php");
 require_once("php/classes.php");
 
-session_start();
-?>
-
-
-
-<?php
+/**
+ * Instanzen
+ */
 $admin=new dozent();
+
+/**
+ * Session wird gestartet
+ */
+session_start();
+
+/**
+ * Aktuelleer User wird ausgelesen
+ */
 $adminInstnc=$admin->getById($_SESSION['id']);
+
+/**
+ * Userliste für Admin wird ausgelesen
+ */
 $userlist=$admin->getAll($_SESSION['id']);
 
+/**
+ * Sicherheitsabfrage
+ */
 if(!isset($_SESSION['login'])):
     header ('location: index.php');
-    ?>
 
-<?php else:?>
-    <body>
-    <?php require_once("include/navigation.php");
+else:
+    require_once("include/navigation.php");
     ?>
 
     <div class="container">
         <?php
-        //Breadcrumb
+        /**
+         * Breadcrumb
+         */
         echo"
         <div class='breadcrumb'>
         <i class='fa fa-angle-right'></i> <a href='index.php'> Vorlesungen</a> <i class='fa fa-angle-right'></i> Accountverwaltung
@@ -38,6 +54,9 @@ if(!isset($_SESSION['login'])):
         <div class="panel panel-default">
             <div class="panel-body">
         <?php
+        /**
+         * Aktueller User wird an dieser Stelle ausgegeben
+         */
         echo "<div class='col-md-6'>";
         echo $adminInstnc['name'] ."<br>". $adminInstnc['vorname'] ."<br>".$adminInstnc['mail']."<br>".$adminInstnc['ID_RECHTE']."<br>";
         echo "</div><div class='col-md-6'>";
@@ -51,30 +70,31 @@ if(!isset($_SESSION['login'])):
         ?>
 
         <?php
+            /**
+             * Rechteprüfung und Ausgabe der zu überprüfenden User
+             */
             foreach ($userlist as $user){
                 echo "<div class='col-md-6'>";
                 echo $user['name'].$user['vorname'].$user['mail'].$user['ID_RECHTE'];
                 echo "</div>";
                 echo "<div class='col-md-6'>";
-                if ($_SESSION['rights']>$user['ID_RECHTE']) {
-                    if (empty($user['ID_RECHTE'])) {
+                if ($_SESSION['rights']>$user['ID_RECHTE']):
+                    if (empty($user['ID_RECHTE'])):
                         echo "<a href='do/admin_update.php?id=" . $user['ID'] . "&rights=1' class='btn btn-default'>Freischalten</a>";
-                    }
+                    endif;
 
-
-
-                    if ($user['ID_RECHTE']==1){
+                    if ($user['ID_RECHTE']==1):
                         echo "<a href='do/admin_update.php?id=" . $user['ID'] . "&rights=2 'class='btn btn-default'>Grant Admin</a>";
-
-                    } elseif($user['ID_RECHTE']>1) {
+                    elseif($user['ID_RECHTE']>1):
                         echo "<a href='do/admin_update.php?id=" . $user['ID'] . "&rights=1 'class='btn btn-default'>Take Admin</a>";
-                    }
+                    endif;
 
                     echo "<a href='do/admin_delete.php?id=" . $user['ID'] . "'class='btn btn-default'>Löschen</a>";
 
-                } else {
+                else:
                     echo "<a href='' class='btn btn-default btn-doppel'>ACCESS DENIED!</a>";
-                }
+
+                endif;
                 echo "</div>" ;
             }
         }
