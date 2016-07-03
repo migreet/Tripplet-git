3073c911be96e47534cdad0217558766f8de17a1
migreet <mic.graeter@googlemail.com>
2016-07-03 21:51:31 +0200

    code aufräumen

--- beamer.php
+++ beamer.php
@@ -8,36 +8,42 @@
 
 require_once("include/header.php");
 require_once("php/classes.php");
-session_start();
-//Instanzen
+
+/**
+ * Instanzen
+ */
 $antwortInstnc = new antwort();
 $votingInstnc = new voting();
 $frageInstnc = new frage();
 $auswertungInstnc = new auswertung();
 
-
+/**
+ * GETs
+ */
 $ID_Frage=$_GET['id'];
+
+/**
+ * Session starten
+ */
+session_start();
+
 $frage=$frageInstnc->getById($ID_Frage);
 
-
-//Rights Check
+/**
+ * Rights Check
+ */
 $usercheck=$frageInstnc->userCheck($ID_Frage);
 if($usercheck['ID']!=$_SESSION['user_id']) {
     header ('location: index.php');
 }
-print_r($usercheck);
-print_r($_SESSION);
 
 echo "<div id='beamer' class='container'>";
+$anzahlTeilnehmer=$auswertungInstnc->countTeilnehmer($frage['ID']);
+echo "<h2>" . $frage['text'] . "(" . $anzahlTeilnehmer['COUNT(*)'] . " Teilnehmer)"."</h2></br> ";
 
-            $anzahlTeilnehmer=$auswertungInstnc->countTeilnehmer($frage['ID']);
-            echo "<h2>" . $frage['text'] . "(" . $anzahlTeilnehmer['COUNT(*)'] . " Teilnehmer)"."</h2></br> ";
-
-            ?>
-
-            <!--Ausgabe der Antworten zu der passenden Frage-->
-
-            <?php
+/**
+ * Ausgabe der Antworten zu der passenden Frage
+ */
             $antwort=$antwortInstnc->getByFragenId($frage['ID']);
             foreach ($antwort as $eintragFrage) {
                 $anzahlAntworten=$auswertungInstnc->countAntworten($eintragFrage['ID']);
@@ -49,12 +55,11 @@ echo "<div id='beamer' class='container'>";
                 echo "
                 <div class='col-md-6 progress' style='margin-top: 20px; margin-bottom: 10px;height:40px; padding-left: 0; padding-right: 0;'>
                     <div class='progress-bar progress-bar-info' role='progressbar' aria-valuenow='$percent' aria-valuemin='0' aria-valuemax='100' style='background-color:#55d6ba; width: $percent%'>
-                <span class='sr-only'>20% Complete</span>
+                        <span class='sr-only'>20% Complete</span>
+                    </div>
                 </div>
-                </div>
-                </div>
+        </div>
                 ";
 
             }
             ?>
-        </div>
