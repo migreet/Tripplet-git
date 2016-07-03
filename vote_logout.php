565aee784243d1ed04ea0420d2185734c2f07473
migreet <mic.graeter@googlemail.com>
2016-07-03 23:48:27 +0200

    code aufräumen

--- vote_logout.php
+++ vote_logout.php
@@ -4,14 +4,23 @@
  * User: Andreas
  * Date: 24.04.2016
  * Time: 15:10
+ * Ausloggen beim Voting
  */
 
+/**
+ * Requires
+ */
 require_once("include/header.php");
 require_once("php/classes.php");
 
+/**
+ * Session wird gestartet
+ */
 session_start();
 
+/**
+ * Session wird gecleaned
+ */
 unset($_SESSION['votingid']);
-
 header('location: vote.php?notification='."2");
 ?>
