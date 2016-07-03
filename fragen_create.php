40ac4b0c52c07cfedc809156414001ef7def9dac
migreet <mic.graeter@googlemail.com>
2016-07-03 22:03:38 +0200

    code aufräumen

--- fragen_create.php
+++ fragen_create.php
@@ -4,6 +4,7 @@
  * User: Mic
  * Date: 05.04.2016
  * Time: 11:33
+ * Fragen erstellen
  */
 ?>
 
@@ -11,38 +12,47 @@
 <?php
 require_once("include/header.php");
 require_once("php/classes.php");
-session_start();
 
+/**
+ * GETs
+ */
+$postFragen=$_POST["fragencreate"];
+
+/**
+ * Instanzen
+ */
 $frageInstnc = new frage();
+
+/**
+ * Session wird gestartet
+ */
+session_start();
 ?>
 
 <div class="container">
-
-
     <h1> Fragen von Voting xyz</h1>
     <div class="col-md-8">
         <?php
-
-
-
+        /**
+         *
+         */
         $fragen = $frageInstnc->getByVotingId($_GET ['id']);
-        if (!empty ($fragen)) {
+        if (!empty ($fragen)):
             foreach ($fragen as $eintrag) {
                 echo "<div class='list-entry'><a href=vorlesung.php?id=" . $eintrag['ID'] . ">";
                 echo $eintrag['bezeichnung'] . " ";
                 echo "</a> </div>";
             }
-        }
-        else {
+        else:
             echo "Es sind keine Fragen vorhanden";
-        }
+        endif;
         ?>
+
     </div>
     <div class="col-md-4">
         <div class="col-sm-12">
             <p><strong>Fügen sie eine neue Fragen zum Voting hinzu</strong></p>
         </div>
-
         <form class="form-horizontal" role="form" action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
             <div class="form-group">
                 <div class="col-sm-12">
@@ -56,30 +66,29 @@ $frageInstnc = new frage();
                     <input type="hidden" value="1" name="fragencreate">
                 </div>
             </div>
-
-
             <div class="form-group">
                 <div class="col-sm-12">
                     <button type="submit" class="btn btn-default">Fragen hinzufügen</button>
                 </div>
             </div>
         </form>
+
         <?php
-        $postFragen=$_POST["fragencreate"];
-        if (isset($postFragen)) {
+
+        if (isset($postFragen)):
             $frage1 = htmlspecialchars($_POST["frage1"], ENT_QUOTES, "UTF-8");
 
-
-            if (!empty ($frage1)) {
+            if (!empty ($frage1)):
                 $frageInstnc = new frage();
                 $frage = $frageInstnc->createVorlesung($bezeichnung, $_SESSION ['id']);
 
                 echo "<div> Die Fragen wuden dem Voting erfolgreich hinzugefügt!</div>";
                 header('Location: index.php');
-            }
-            else {echo "<div> Hinzufügen nicht erfolgreich! Wenden Sie sich bitte an den Administrator.</div>";}
-        }
 
+            else: echo "<div> Hinzufügen nicht erfolgreich! Wenden Sie sich bitte an den Administrator.</div>";
+            endif;
+
+        endif;
         ?>
     </div>
 
