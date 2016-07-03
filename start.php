2d93988aac53dfc09a8b06bdb3e4c59533e8b02b
migreet <mic.graeter@googlemail.com>
2016-07-03 23:26:21 +0200

    code aufräumen

--- start.php
+++ start.php
@@ -43,115 +43,117 @@ $schluesselcheck=$votingInstnc->getByKey($schluessel);
  * Sicherheitsabfrage
  */
 if(!isset($_SESSION['login'])):
-
-require_once("include/navigation_login.php");
-
-/**
- * Rights Check
- */
-$usercheck=$votingInstnc->userCheck($votingId);
-if($usercheck['ID']!=$_SESSION['user_id']):
     header ('location: index.php');
-endif;
 
-/**
- * Ifabfrage für Schlüsseleingabe und Statusprüfung
- */
-if (isset($votingsent)) {
-//Wenn Voting nicht gestartet UND Schlüssel ausgefüllt
-    if (empty($voting['schluessel'])){
-        if (empty($schluesselcheck)){
-            $voting = $votingInstnc->update($votingId, $schluessel);
-            header('Location: start.php?id='.$votingId."&notification=4");
+else:
+    require_once("include/navigation_login.php");
+
+    /**
+     * Rights Check
+     */
+    $usercheck=$votingInstnc->userCheck($votingId);
+    if($usercheck['ID']!=$_SESSION['user_id']):
+        header ('location: index.php');
+    endif;
+
+    /**
+     * Ifabfrage für Schlüsseleingabe und Statusprüfung
+     */
+    if (isset($votingsent)) {
+    //Wenn Voting nicht gestartet UND Schlüssel ausgefüllt
+        if (empty($voting['schluessel'])){
+            if (empty($schluesselcheck)){
+                $voting = $votingInstnc->update($votingId, $schluessel);
+                header('Location: start.php?id='.$votingId."&notification=4");
+            }
+            else{
+                header('Location: start.php?id='.$votingId."&notification=6");
+            }
+
+            }
+        else {
+            $voting = $votingInstnc->update($votingId, NULL);
+            header('Location: start.php?id='.$votingId."&notification=5");
         }
-        else{
-            header('Location: start.php?id='.$votingId."&notification=6");
         }
 
-        }
-    else {
-        $voting = $votingInstnc->update($votingId, NULL);
-        header('Location: start.php?id='.$votingId."&notification=5");
-    }
-    }
-
-require_once("include/navigation.php");
-?>
-
-
-
-<div class="container">
-    <?php
-    //Breadcrumb
-    $vorlesung = $vorlesungInstnc->getById($voting['ID_VORLESUNG']);
-    echo"
-        <div class='breadcrumb'>
-        <i class='fa fa-angle-right'></i> <a href='index.php'>Vorlesungen</a> <a href='vorlesung.php?id=".$vorlesung['ID'] ."'>". $vorlesung['bezeichnung']. "</a> <i class='fa fa-angle-right'></i> ". $voting['bezeichnung'] ." starten
-        </div>";
+    require_once("include/navigation.php");
     ?>
-<h1>Voting <?php echo $voting['bezeichnung']; ?> starten</h1>
-    <div class="col-md-4">
-        <?php require_once('include/aside_start.php') ?>
+
+
+
+    <div class="container">
+        <?php
+        //Breadcrumb
+        $vorlesung = $vorlesungInstnc->getById($voting['ID_VORLESUNG']);
+        echo"
+            <div class='breadcrumb'>
+            <i class='fa fa-angle-right'></i> <a href='index.php'>Vorlesungen</a> <a href='vorlesung.php?id=".$vorlesung['ID'] ."'>". $vorlesung['bezeichnung']. "</a> <i class='fa fa-angle-right'></i> ". $voting['bezeichnung'] ." starten
+            </div>";
+        ?>
+    <h1>Voting <?php echo $voting['bezeichnung']; ?> starten</h1>
+        <div class="col-md-4">
+            <?php require_once('include/aside_start.php') ?>
+            </div>
+        <div class="col-md-8">
+
+    <form name="registerform" class="form-horizontal" role="form" action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+
+
+                <input type="hidden" value="1" name="votingstart">
+
+
+    <?php //Buttonänderung
+    if (empty ($voting['schluessel'])):
+    ?>
+        <div class="form-group">
+            <input type="text" class="form-control" name="schluessel" placeholder="Schlüssel" id="schluessel" required>
         </div>
-    <div class="col-md-8">
+        <div class="form-group">
+                <button type="submit" name="start" class="btn btn-success">starten</button>
 
-<form name="registerform" class="form-horizontal" role="form" action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+        </div>
+        <?php
 
-
-            <input type="hidden" value="1" name="votingstart">
-
-
-<?php //Buttonänderung
-if (empty ($voting['schluessel'])):
-?>
-    <div class="form-group">
-        <input type="text" class="form-control" name="schluessel" placeholder="Schlüssel" id="schluessel" required>
-    </div>
-    <div class="form-group">
-            <button type="submit" name="start" class="btn btn-success">starten</button>
-
-    </div>
-    <?php
-
-    if ($_GET['notification']=="5") {
+        if ($_GET['notification']=="5") {
+                echo "<div class='notifikation'>";
+                echo "Das Voting wurde beendet.";
+                echo "</div>";
+            }
+        elseif ($_GET['notification']=="6") {
             echo "<div class='notifikation'>";
-            echo "Das Voting wurde beendet.";
+            echo "Dieser Schlüssel wird im Moment leider bereits verwendet!";
             echo "</div>";
         }
-    elseif ($_GET['notification']=="6") {
-        echo "<div class='notifikation'>";
-        echo "Dieser Schlüssel wird im Moment leider bereits verwendet!";
-        echo "</div>";
-    }
-     ?>
-
-<?php
-else:
-?>
-
-    <div class="form-group">
-            <button type="submit" name="stop" class="btn btn-danger">stoppen</button>
-    </div>
+         ?>
 
     <?php
-    if ($_GET['notification']=="4") {
-    echo "<div class='notifikation'>";
-    echo "Das Voting wurde erfolgreich gestartet.";
-    echo "</div>";
-    }
+    else:
+    ?>
+
+        <div class="form-group">
+                <button type="submit" name="stop" class="btn btn-danger">stoppen</button>
+        </div>
+
+        <?php
+        if ($_GET['notification']=="4") {
+        echo "<div class='notifikation'>";
+        echo "Das Voting wurde erfolgreich gestartet.";
+        echo "</div>";
+        }
 
 
 
 
-endif;
-?>
-</form>
-</div>
-
+    endif;
+    ?>
+    </form>
     </div>
-</div>
 
-<?php require_once('include/footer.php');
+        </div>
+    </div>
+
+    <?php require_once('include/footer.php');
 
 endif;
 ?>
