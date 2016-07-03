b24c7f88e5aa1e30bdbcf8cbf93e0ed3019f28a5
migreet <mic.graeter@googlemail.com>
2016-07-03 15:20:36 +0200

    code aufräumen

--- include/aside_vorlesung.php
+++ include/aside_vorlesung.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Mic
+ * Date: 21.04.2016
+ * Time: 20:25
+ * Sidebar Vorlesung
+ */
+
+/**
+ * Sicherheitsabfrage
+ * Formular zum Voting erstellen
+ */
 if(!isset($_SESSION['login'])):
     header ('location: ../index.php');
 
@@ -7,33 +19,23 @@ else:
 
 <div class="panel panel-default">
     <div class="panel-body">
-
         <div class='side-text'>
             <p>Hier steht ein Platzhaltertext</p>
         </div>
-
-<div class="sidebar-left">
-<form class="form-horizontal" role="form" action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
-            <div class="form-group">
-                <div class="col-sm-12">
-                    <input type="text" class="form-control" name="bezeichnung" placeholder="Bezeichnnung" id="bezeichnung" required>
+        <div class="sidebar-left">
+            <form class="form-horizontal" role="form" action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+                <div class="form-group">
+                    <div class="col-sm-12">
+                        <input type="text" class="form-control" name="bezeichnung" placeholder="Bezeichnnung" id="bezeichnung" required>
+                    </div>
+                        <input type="hidden" value="1" name="votingcreate">
+                        <div class="col-sm-12">
+                            <button type="submit" class="btn btn-default">hinzufügen</button>
+                        </div>
                 </div>
-                <input type="hidden" value="1" name="votingcreate">
-
-
-
-
-
-
-                <div class="col-sm-12">
-                    <button type="submit" class="btn btn-default">hinzufügen</button>
-                </div>
-            </div>
-        </form>
-
-
-</div>
+            </form>
         </div>
     </div>
+</div>
 
 <?php endif; ?>
