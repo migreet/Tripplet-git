6da802e2d6c66dc46957aeba5970f6c10fa97704
migreet <mic.graeter@googlemail.com>
2016-07-04 16:36:32 +0200

    code feinschliff

--- include/aside_start.php
+++ include/aside_start.php
@@ -4,7 +4,7 @@
     <div class="panel-body">
         <div class='sidebar-left'>
             <div class='side-text'>
-                Öffne Votings indem du einen Schlüssel vergibst und auf <strong>starten</strong> drückst. Mit diesem Schlüssel kann man unter <strong>https://mars.iuk.hdm-stuttgart.de/~mg144/vote.php</strong> und der Eingabe des korrekten schlüssels am Voting teilnehmen. Votings werden nach 90 Minuten automatisch geschlossen.
+                Öffne Votings indem du einen Schlüssel vergibst und auf <strong>starten</strong> drückst. Mit diesem Schlüssel kann man unter <br/><strong>https://mars.iuk.hdm-stuttgart.de/~mg144/vote.php</strong><br/> und der Eingabe des korrekten schlüssels am Voting teilnehmen. Votings werden nach 90 Minuten automatisch geschlossen.
             </div>
         </div>
     </div>
