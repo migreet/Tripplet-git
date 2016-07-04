a20e91cefb3609107e7b09af45cae86866b8ee23
migreet <mic.graeter@googlemail.com>
2016-07-04 15:22:55 +0200

    code feinschliff

--- include/header.php
+++ include/header.php
@@ -8,5 +8,5 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">
     <link rel = "stylesheet" href = "./css/style.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    <title>Trippletime</title>
+    <title>Snap</title>
 </head>
