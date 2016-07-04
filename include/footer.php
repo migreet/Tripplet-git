79044e219e6c0e605a300d31ab349b2a3eb444f2
migreet <mic.graeter@googlemail.com>
2016-07-04 19:35:29 +0200

    code feinschliff

--- include/footer.php
+++ include/footer.php
@@ -1,6 +1,8 @@
 <!-- Footer -->
 
 <div class="footer">
+        <div class="col-md-offset-10 col-md-2">
         <a href="./impressum.php">Impressum</a>
         <a href="./kontakt.php">Kontakt</a>
+        </div>
 </div>
