28c705888dd3854d07da7c7fd2e159238f2fb9d8
migreet <mic.graeter@googlemail.com>
2016-07-04 16:08:48 +0200

    code feinschliff

--- include/navigation.php
+++ include/navigation.php
@@ -3,9 +3,12 @@
 <nav class="navbar navbar-inverse">
     <div class="container">
         <ul class="nav navbar-nav" style="float: right">
-            <li>
-                <a href="admin.php">Accountverwaltung</a>
-            </li>
+            <?php
+            if ($_SESSION['rights']<1):
+                echo "<li>
+                        <a href="admin.php">Accountverwaltung</a>
+                      </li>";
+            endif; ?>
             <li>
                 <a href="logout.php">Logout</a>
             </li>
