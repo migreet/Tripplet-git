b3a0ccaf6def7a521e9569d2608c974d830ec484
migreet <mic.graeter@googlemail.com>
2016-07-04 18:42:44 +0200

    code feinschliff

--- vorlesung.php
+++ vorlesung.php
@@ -77,7 +77,7 @@ endif;
     <!-- Breadcrumb -->
     <div class='breadcrumb'>
         <i class='fa fa-angle-right'></i> <a href='index.php'>Vorlesungen</a> <i class='fa fa-angle-right'></i>
-        <?php echo $vorlesung['bezeichnung']; ?>
+        <?php  echo $vorlesung['bezeichnung']; ?>
     </div>
     <h1> <?php echo "Votingübersicht " . $vorlesung['bezeichnung']; ?></h1>
     <div class="col-md-4 sidebar">
