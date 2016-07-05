d2bdf69b23817846f640af13df5586b5ebdcde93
BlueberryCrunch <a.j.muehlbauer@gmail.com>
2016-07-05 19:34:39 +0200

    styling login form vote.php

--- kontakt.php
+++ kontakt.php
@@ -22,9 +22,10 @@ session_start();
         <i class='fa fa-angle-right'></i> <a href='index.php'> Vorlesungen</a> <i class='fa fa-angle-right'></i> Kontakt
     </div>
     <h1>Impressum</h1>
-    echo "<div class='col-md-4' id='impressim_aside'> ";
+    <?php echo "<div class='col-md-4' id='impressim_aside'> ";
         require_once("include/aside_impressum.php");
         echo "</div>";
+    ?>
     <div class='col-md-8'>
     </div>
 </div>
