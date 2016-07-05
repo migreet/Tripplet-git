d2492b9231bdf0ae305bde02fd7848701eecf78d
BlueberryCrunch <a.j.muehlbauer@gmail.com>
2016-07-05 16:04:20 +0200

    styling login form vote.php

--- include/aside_auswertung.php
+++ include/aside_auswertung.php
@@ -3,6 +3,7 @@
 <div class="panel panel-default">
     <div class="panel-body">
         <div class='sidebar-left'>
+            <img src="img/snap-aufeinenblick.svg">
             <div class='side-text'>
                 Das Ergebnis deines Votings wird prozentual und absolut angezeigt. Mit <strong>Beam me up!</strong> kannst du die Resultate jeder einzelnen Frage auf einer Beamerfreundlichen Seite öffnen.
             </div>
